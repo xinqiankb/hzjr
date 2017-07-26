0a70bc17d4194f8b2a51f43437cd6ef3e32cfb3e
xinqiankb <kangbo_1991@126.com>
2017-07-26 13:00:55 +0800

    'xinqian'

---
+++ src/components/article/index.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="article" id="article">
+		<HeaderNavbar></HeaderNavbar>
+		<Content></Content>
+	</div>
+</template>
+
+<script>
+	import Content from './content'
+	import HeaderNavbar from './../header'
+	export default{
+		name:'article',
+		components:{
+			Content,HeaderNavbar
+		},
+		data(){
+			return {
+
+			}
+		}
+
+	}
+
+</script>
