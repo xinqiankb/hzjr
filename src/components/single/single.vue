2ae2ada9a3cc23e0d6ae9eb01556543186025fe3
xinqiankb <kangbo_1991@126.com>
2017-07-26 16:18:23 +0800

    'single_page_and_list_page_and_news'

---
+++ src/components/single/single.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="single" id="single">
+		<HeaderNavbar></HeaderNavbar>
+		<div class="main">
+			<div class="maincontent">
+			</div>
+		</div>
+		<footerBox></footerBox>
+	</div>
+</template>
+
+<script>
+	import HeaderNavbar from './../header'
+	import footerBox from './../footer'
+	export default {
+		name:'single',
+		components:{HeaderNavbar,footerBox},
+		data(){
+			return{
+
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	.single .main{width: 1200px;margin:0 auto;padding-bottom: 4rem}
+	.maincontent{padding: 1rem 0}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import home from '@/components/home/home'
 import article from '@/components/article/index'
+import list from '@/components/list/list'
+import news from '@/components/news/news'
+import single from '@/components/single/single'
 
 Vue.use(Router)
 
@@ -16,6 +19,21 @@ export default new Router({
       path: '/article/:id',
       name: 'article',
       component: article
+    },
+    {
+      path: '/list',
+      name: 'list',
+      component: list
+    },    
+    {
+      path: '/news',
+      name: 'news',
+      component: news
+    },    
+    {
+      path: '/single',
+      name: 'single',
+      component: single
     }
   ]
 })
